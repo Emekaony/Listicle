7ea4be9292c260389a15c4c9e77d12132d873114
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-03-06 21:59:23 -0800

    Dummy component to avoid re-writing boilerplate code

---
+++ dummyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SignUp = () => {
+  return;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignUp;
